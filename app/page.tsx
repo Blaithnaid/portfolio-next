5232f3560fca0dc3817d4d57fe3f9057af06f318
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2025-02-11 13:48:13 +0000

    we have gotten as far as the homepage working!

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,61 @@
+"use client";
+import { useState } from "react";
+// import { usePathname } from "next/navigation";
+import Head from "next/head";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import CVModal from "../components/CVModal";
+import Nav from "@/components/Nav";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+import HomeBg from "@/public/home.webp";
+
+export default function Home() {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	return (
+		<>
+			<Head>
+				<title>home ~ isb</title>
+			</Head>
+			<div
+				className="flex flex-col justify-end items-end p-4 bg-cover bg-center w-full h-[calc(100vh-12.5rem)] md:h-screen"
+				style={{ backgroundImage: `url(${HomeBg.src})` }}
+			>
+				<div className="w-full md:w-[500px] min-h-[15rem] text-left text-lg relative">
+					<div className="absolute inset-0 bg-[#253956] bg-blend-color-burn mix-blend-color-burn"></div>
+					<div className="flex flex-col gap-1 border-2 border-white border-dashed justify-between relative z-10 p-4 h-full hover:cursor-default">
+						<p>
+							<b>hi! i'm iarla.</b> i'm a software developer and
+							sound engineer. this is a little portfolio website i
+							built to show off some of my work and advertise my
+							skills!
+						</p>
+						<p>
+							feel free to have a click around to find out what
+							i'm up to, or head straight to my contact page (the
+							little envelope in the navbar!) if you want to get
+							in touch.
+						</p>
+						<p>
+							just interested in seeing my cv? you can take a look
+							<b>
+								{" "}
+								<span
+									className="text-slate-300 hover:text-neonpurple underline cursor-pointer"
+									onClick={() => setIsModalOpen(true)}
+								>
+									here
+								</span>
+							</b>
+							.
+						</p>
+					</div>
+				</div>
+			</div>
+			<CVModal
+				isOpen={isModalOpen}
+				onClose={() => setIsModalOpen(false)}
+			/>
+		</>
+	);
 }
---
+++ components/Nav.tsx
@@ -0,0 +1,109 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+	HomeIcon as HomeSolid,
+	BriefcaseIcon as BriefcaseSolid,
+	EnvelopeIcon as EnvelopeSolid,
+	InformationCircleIcon as InformationCircleSolid,
+} from "@heroicons/react/24/solid";
+import {
+	HomeIcon as HomeOutline,
+	BriefcaseIcon as BriefcaseOutline,
+	EnvelopeIcon as EnvelopeOutline,
+	InformationCircleIcon as InformationCircleOutline,
+} from "@heroicons/react/24/outline";
+
+const navigation = [
+	{
+		name: "home",
+		href: "/home",
+		solidIcon: HomeSolid,
+		outlineIcon: HomeOutline,
+	},
+	{
+		name: "work",
+		href: "/work",
+		solidIcon: BriefcaseSolid,
+		outlineIcon: BriefcaseOutline,
+	},
+	{
+		name: "about me",
+		href: "/about",
+		solidIcon: InformationCircleSolid,
+		outlineIcon: InformationCircleOutline,
+	},
+	{
+		name: "contact",
+		href: "/contact",
+		solidIcon: EnvelopeSolid,
+		outlineIcon: EnvelopeOutline,
+	},
+];
+
+const pageTitles = {
+	"/home": "home",
+	"/work": "my work",
+	"/about": "about me",
+	"/contact": "contact",
+};
+
+export default function Nav() {
+	const pathname = usePathname() as keyof typeof pageTitles;
+	const [hoveredItem, setHoveredItem] = useState<string | null>(null);
+	const [currentPage, setCurrentPage] = useState("");
+
+	useEffect(() => {
+		setCurrentPage(pageTitles[pathname] || "home");
+	}, [pathname]);
+
+	return (
+		<div className="flex flex-col justify-between min-w-80 bg-gunmetal md:h-screen">
+			<div className="cursor-default font-jersey p-3 text-4xl text-center">
+				<div className="mb-0.5">
+					<Link
+						className="text-coral cursor-pointer"
+						href="/home"
+						title="return to home"
+					>
+						<b>iarla sparrow burke</b>
+					</Link>
+				</div>
+				<div className="bg-paynesgray h-0.5 w-full rounded-2xl"></div>
+				<div className="text-silver font-poppins text-sm mt-0.5">
+					software developer ~ sound engineer
+				</div>
+			</div>
+			<div className="flex flex-nowrap flex-col shrink items-center justify-center align-center pb-5">
+				<div className="text-white text-2xl font-poppins cursor-default underline">
+					<b>{currentPage}</b>
+				</div>
+				<div className="flex flex-row items-center gap-2 mt-5">
+					{navigation.map((item) => (
+						<Link
+							key={item.name}
+							href={item.href}
+							className="font-poppins text-white hover:underline hover:text-white flex items-center gap-2"
+							onMouseEnter={() => setHoveredItem(item.name)}
+							onMouseLeave={() => setHoveredItem(null)}
+						>
+							{hoveredItem === item.name ? (
+								<item.outlineIcon
+									className="h-10 w-10"
+									title={item.name}
+								/>
+							) : (
+								<item.solidIcon className="h-10 w-10" />
+							)}
+						</Link>
+					))}
+				</div>
+			</div>
+			<div className="h-0 md:h-12 text-silver cursor-default text-center">
+				site by me ~{" "}
+				<a className="cursor-pointer underline">view on github</a>
+			</div>
+		</div>
+	);
+}
