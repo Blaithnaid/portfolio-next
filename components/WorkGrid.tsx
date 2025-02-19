cd3940ade2b61999afb2e0462e13099901327109
Blaithnaid <iarlaken@gmail.com>
2025-02-19 18:10:42 +0000

    refactor to use WorkItem

--- components/WorkGrid.tsx
+++ components/WorkGrid.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import Carousel from "./Carousel";
+import WorkItem from "./WorkItem";
 import projects from "@/public/projects.json";
 
 export default function WorkGrid() {
@@ -13,79 +13,21 @@ export default function WorkGrid() {
 	});
 
 	return (
-		<>
-			<div className="bg-slate-600 bg-opacity-70 p-2 h-max w-full flex flex-col justify-start items-center">
-				<div className="bg-gunmetal my-4 md:my-8 shadow-lg shadow-gunmetal h-16 rounded-xs w-full md:max-w-[30rem] md:w-full">
-					<input
-						type="text"
-						placeholder="search projects..."
-						value={searchTerm}
-						onChange={(e) => setSearchTerm(e.target.value)}
-						className="w-full h-full bg-transparent text-silver px-4 focus:outline-hidden"
-					/>
-				</div>
-				<div className="max-h-max min-h-64 w-full md:px-10 lg:px-20 grid grid-cols-1 auto-rows-max lg:grid-cols-2 gap-8 p-2">
-					{projectsFiltered.map((project, index) => {
-						const slides = project.images.map((image) => ({
-							type: "image" as const,
-							src: image,
-						}));
-
-						return (
-							<div
-								key={index}
-								className="w-full flex flex-col rounded-xs p-0 bg-gunmetal shadow-card shadow-gray-600"
-							>
-								<div className="w-full aspect-3/2 bg-coral overflow-hidden rounded-t-sm relative">
-									<Carousel items={slides} />
-								</div>
-								<div className="flex flex-col gap-1 cursor-default justify-evenly text-xl py-3 bg-paynesgray font-bold text-center">
-									{project.title}
-									<div className="text-silver flex justify-center items-center font-jersey font-light px-3 text-pretty">
-										-
-										<i className="mx-1.5">
-											{project.subheading}
-										</i>
-										-
-									</div>
-								</div>
-								<div className="flex flex-col justify-between flex-1">
-									<div className="px-4 text-pretty font-poppins font-normal text-left p-3">
-										{project.description}
-									</div>
-									<div className="mt-auto">
-										<div className="flex flex-row gap-1 px-4 pt-2 text-pretty font-poppins text-left font-normal">
-											{project.links?.map(
-												(link, index) => (
-													<a
-														key={index}
-														href={link.url}
-														target="_blank"
-														rel="noreferrer"
-														className="bg-slate-600 bg-opacity-70 p-2 rounded-xs hover:bg-coral transition-colors duration-150"
-													>
-														{link.name}
-													</a>
-												)
-											)}
-										</div>
-										<div className="flex flex-row flex-wrap gap-1 px-4 pb-4 pt-2 text-pretty font-poppins font-light text-left text-sm">
-											{project.tags.map((tag, index) => (
-												<span
-													key={index}
-													className="bg-slate-100 text-black bg-opacity-70 px-1 py-0.5 rounded-xs"
-												>
-													{tag}
-												</span>
-											))}
-										</div>
-									</div>
-								</div>
-							</div>
-						);
-					})}
-				</div>
+		<div className="bg-slate-600 bg-opacity-70 p-2 h-max w-full flex flex-col justify-start items-center">
+			<div className="bg-gunmetal my-4 md:my-8 shadow-lg shadow-gunmetal h-16 rounded-xs w-full md:max-w-[30rem] md:w-full">
+				<input
+					type="text"
+					placeholder="search projects..."
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}
+					className="w-full h-full bg-transparent text-silver px-4 focus:outline-hidden"
+				/>
 			</div>
-		</>
+			<div className="max-h-max min-h-64 w-full md:px-10 lg:px-20 grid grid-cols-1 auto-rows-max lg:grid-cols-2 gap-8 p-2">
+				{projectsFiltered.map((project, index) => (
+					<WorkItem key={index} {...project} />
+				))}
+			</div>
+		</div>
 	);
 }
--- components/WorkItem.tsx
+++ components/WorkItem.tsx
@@ -11,24 +11,24 @@ interface Project {
 }
 
 export default function WorkItem(project: Project) {
-	const slides = project.images.map((image) => ({
-		type: "image" as const,
-		src: image,
-	}));
-
 	return (
 		<div className="w-full flex flex-col rounded-xs p-0 bg-gunmetal shadow-card shadow-gray-600">
 			<div className="w-full aspect-3/2 bg-coral overflow-hidden rounded-t-sm relative">
-				<Carousel items={slides} />
+				<Carousel
+					items={project.images.map((image) => ({
+						type: "image" as const,
+						src: image,
+					}))}
+				/>
 			</div>
-			<div className="flex flex-col gap-1 cursor-default justify-evenly text-xl py-3 bg-paynesgray text-white font-bold text-center">
+			<div className="flex flex-col gap-1 cursor-default justify-evenly text-xl py-3 bg-paynesgray font-bold text-center">
 				{project.title}
 				<div className="text-silver flex justify-center items-center font-jersey font-light px-3 text-pretty">
 					-<i className="mx-1.5">{project.subheading}</i>-
 				</div>
 			</div>
 			<div className="flex flex-col justify-between flex-1">
-				<div className="px-4 text-pretty font-poppins text-white font-normal text-left p-3">
+				<div className="px-4 text-pretty font-poppins font-normal text-left p-3">
 					{project.description}
 				</div>
 				<div className="mt-auto">
@@ -39,7 +39,7 @@ export default function WorkItem(project: Project) {
 								href={link.url}
 								target="_blank"
 								rel="noreferrer"
-								className="bg-slate-600 bg-opacity-70 text-white p-2 rounded-xs hover:bg-coral"
+								className="bg-slate-600 bg-opacity-70 p-2 rounded-xs hover:bg-coral transition-colors duration-150"
 							>
 								{link.name}
 							</a>
@@ -49,7 +49,7 @@ export default function WorkItem(project: Project) {
 						{project.tags.map((tag, index) => (
 							<span
 								key={index}
-								className="bg-slate-100 text-black bg-opacity-70 px-1 py-0.5 rounded-xs"
+								className="bg-slate-300 text-black bg-opacity-70 px-1 py-0.5 rounded-xs"
 							>
 								{tag}
 							</span>
