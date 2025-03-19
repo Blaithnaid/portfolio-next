cb3366fc885fd85850e8769402a868e7e23b0dee
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2025-03-19 18:27:01 +0000

    reformat, make X icon larger in lightbox

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -5,103 +5,99 @@ import Image from "next/image";
 import { XMarkIcon } from "@heroicons/react/16/solid";
 
 type CarouselItem = {
-	type: "image" | "video";
-	src: string;
-	alt?: string;
+  type: "image" | "video";
+  src: string;
+  alt?: string;
 };
 
 interface CarouselProps {
-	items: CarouselItem[];
-	initialIndex?: number;
+  items: CarouselItem[];
+  initialIndex?: number;
 }
 
 export default function Carousel({ items, initialIndex = 0 }: CarouselProps) {
-	const [currentIndex, setCurrentIndex] = useState(initialIndex);
-	const [lightboxOpen, setLightboxOpen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(initialIndex);
+  const [lightboxOpen, setLightboxOpen] = useState(false);
 
-	const currentItem = items[currentIndex];
+  const currentItem = items[currentIndex];
 
-	return (
-		<>
-			{/* Carousel container */}
-			<div className="relative w-full h-full flex flex-col items-center justify-center bg-black">
-				{/* Main item */}
-				<div className="relative w-full h-full flex items-center justify-center">
-					{currentItem.type === "image" ? (
-						<Image
-							src={currentItem.src}
-							alt={currentItem.alt || ""}
-							fill
-							style={{ objectFit: "cover" }}
-							sizes="100vw"
-							onClick={() => setLightboxOpen(true)}
-							className="hover:cursor-pointer"
-						/>
-					) : (
-						<iframe
-							src={currentItem.src}
-							title="YouTube video"
-							frameBorder="0"
-							allowFullScreen
-							className="w-full h-full"
-						/>
-					)}
-				</div>
-				<div className="h-fit w-full bg-gunmetal flex items-center">
-					<div className="flex justify-center w-full h-6 p-0 ">
-						{items.map((_, index) => (
-							<button
-								key={index}
-								onClick={() => setCurrentIndex(index)}
-								className="group grow relative cursor-pointer bg-transparent border-none p-0 first:pl-2 last:pr-2 focus:outline-hidden"
-								aria-label={`Go to slide ${index + 1}`}
-							>
-								{/* The circle is absolutely centered, but the parent is fully clickable */}
-								<div
-									className={`
+  return (
+    <>
+      {/* Carousel container */}
+      <div className="relative w-full h-full flex flex-col items-center justify-center bg-black">
+        {/* Main item */}
+        <div className="relative w-full h-full flex items-center justify-center">
+          {currentItem.type === "image" ? (
+            <Image
+              src={currentItem.src}
+              alt={currentItem.alt || ""}
+              fill
+              style={{ objectFit: "cover" }}
+              sizes="100vw"
+              onClick={() => setLightboxOpen(true)}
+              className="hover:cursor-pointer"
+            />
+          ) : (
+            <iframe
+              src={currentItem.src}
+              title="YouTube video"
+              allowFullScreen
+              className="w-full h-full"
+            />
+          )}
+        </div>
+        <div className="h-fit w-full bg-gunmetal flex items-center">
+          <div className="flex justify-center w-full h-6 p-0 ">
+            {items.map((_, index) => (
+              <button
+                key={index}
+                onClick={() => setCurrentIndex(index)}
+                className="group grow relative cursor-pointer bg-transparent border-none p-0 first:pl-2 last:pr-2 focus:outline-hidden"
+                aria-label={`Go to slide ${index + 1}`}
+              >
+                <div
+                  className={`
 										absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
 										w-[95%] h-2.5 rounded-full
 										${index === currentIndex ? "bg-white" : "bg-white/30"}
 										group-hover:bg-white/50 transition-colors duration-75
 									`}
-								/>
-							</button>
-						))}
-					</div>
-				</div>
-			</div>
+                />
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
 
-			{/* Lightbox Overlay */}
-			{lightboxOpen && (
-				<div className="fixed inset-0 z-20 flex items-center justify-center bg-black/80">
-					<button
-						className="absolute top-10 right-10 text-white hover:text-gray-300"
-						onClick={() => setLightboxOpen(false)}
-						aria-label="Close Lightbox"
-					>
-						<XMarkIcon className="h-8 w-8" />
-					</button>
-					<div className="relative w-[80vw] h-[80vh]">
-						{currentItem.type === "image" ? (
-							<Image
-								src={currentItem.src}
-								alt={currentItem.alt || ""}
-								fill
-								style={{ objectFit: "contain" }}
-								sizes="90vw"
-							/>
-						) : (
-							<iframe
-								src={currentItem.src}
-								title="YouTube video"
-								frameBorder="0"
-								allowFullScreen
-								className="w-full h-full"
-							/>
-						)}
-					</div>
-				</div>
-			)}
-		</>
-	);
+      {lightboxOpen && (
+        <div className="fixed inset-0 z-20 flex items-center justify-center bg-black/80">
+          <button
+            className="absolute top-10 cursor-pointer right-10 text-white hover:text-gray-300"
+            onClick={() => setLightboxOpen(false)}
+            aria-label="Close Lightbox"
+          >
+            <XMarkIcon className="h-12 w-12" />
+          </button>
+          <div className="relative w-[80vw] h-[80vh]">
+            {currentItem.type === "image" ? (
+              <Image
+                src={currentItem.src}
+                alt={currentItem.alt || ""}
+                fill
+                style={{ objectFit: "contain" }}
+                sizes="90vw"
+              />
+            ) : (
+              <iframe
+                src={currentItem.src}
+                title="YouTube video"
+                allowFullScreen
+                className="w-full h-full"
+              />
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,126 +4,124 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import * as motion from "motion/react-client";
 import {
-	HomeIcon as HomeSolid,
-	BriefcaseIcon as BriefcaseSolid,
-	EnvelopeIcon as EnvelopeSolid,
-	InformationCircleIcon as InformationCircleSolid,
+  HomeIcon as HomeSolid,
+  BriefcaseIcon as BriefcaseSolid,
+  EnvelopeIcon as EnvelopeSolid,
+  InformationCircleIcon as InformationCircleSolid,
 } from "@heroicons/react/24/solid";
 import {
-	HomeIcon as HomeOutline,
-	BriefcaseIcon as BriefcaseOutline,
-	EnvelopeIcon as EnvelopeOutline,
-	InformationCircleIcon as InformationCircleOutline,
+  HomeIcon as HomeOutline,
+  BriefcaseIcon as BriefcaseOutline,
+  EnvelopeIcon as EnvelopeOutline,
+  InformationCircleIcon as InformationCircleOutline,
 } from "@heroicons/react/24/outline";
 
 const navigation = [
-	{
-		name: "home",
-		href: "/",
-		title: "home",
-		solidIcon: HomeSolid,
-		outlineIcon: HomeOutline,
-	},
-	{
-		name: "work",
-		href: "/work",
-		title: "my work",
-		solidIcon: BriefcaseSolid,
-		outlineIcon: BriefcaseOutline,
-	},
-	{
-		name: "about me",
-		href: "/about",
-		title: "about me",
-		solidIcon: InformationCircleSolid,
-		outlineIcon: InformationCircleOutline,
-	},
-	{
-		name: "contact",
-		href: "/contact",
-		title: "contact",
-		solidIcon: EnvelopeSolid,
-		outlineIcon: EnvelopeOutline,
-	},
+  {
+    name: "home",
+    href: "/",
+    title: "home",
+    solidIcon: HomeSolid,
+    outlineIcon: HomeOutline,
+  },
+  {
+    name: "work",
+    href: "/work",
+    title: "my work",
+    solidIcon: BriefcaseSolid,
+    outlineIcon: BriefcaseOutline,
+  },
+  {
+    name: "about me",
+    href: "/about",
+    title: "about me",
+    solidIcon: InformationCircleSolid,
+    outlineIcon: InformationCircleOutline,
+  },
+  {
+    name: "contact",
+    href: "/contact",
+    title: "contact",
+    solidIcon: EnvelopeSolid,
+    outlineIcon: EnvelopeOutline,
+  },
 ];
 
 export default function Nav() {
-	const pathname = usePathname();
-	const [hoveredItem, setHoveredItem] = useState<string | null>(null);
+  const pathname = usePathname();
+  const [hoveredItem, setHoveredItem] = useState<string | null>(null);
 
-	// Find the navigation entry whose href matches the current path
-	const currentNavItem = navigation.find((nav) => nav.href === pathname);
-	const currentPage = currentNavItem?.title || " ";
+  // Find the navigation entry whose href matches the current path
+  const currentNavItem = navigation.find((nav) => nav.href === pathname);
+  const currentPage = currentNavItem?.title || " ";
 
-	return (
-		<div className="flex flex-col justify-between min-w-80 bg-gunmetal md:h-screen">
-			<div className="cursor-default font-jersey p-3 text-4xl text-center">
-				<div className="mb-0.5">
-					<a
-						className="text-coral cursor-pointer"
-						href="/home"
-						title="return to home"
-					>
-						iarla sparrow burke
-					</a>
-				</div>
-				<div className="bg-paynesgray h-0.5 w-full rounded-2xl"></div>
-				<div className="text-silver font-poppins text-sm mt-0.5">
-					software developer ~ sound engineer
-				</div>
-			</div>
-			{/* Page title with animation */}
-			<div className="flex flex-nowrap flex-col shrink items-center overflow-hidden justify-center align-center pb-5">
-				<motion.div
-					className="text-white text-center w-fit text-2xl font-poppins cursor-default underline"
-					key={currentPage}
-					initial={{ translateY: -100 }}
-					animate={{ translateY: 0 }}
-					transition={{ duration: 0.3, type: "spring" }}
-				>
-					<b>{currentPage}</b>
-				</motion.div>
-				<div className="flex flex-row items-center gap-2 mt-5">
-					{navigation.map((item) => (
-						<Link
-							key={item.name}
-							href={item.href}
-							className="font-poppins text-white flex items-center gap-2 hover:underline hover:text-white relative"
-							onMouseEnter={() => setHoveredItem(item.name)}
-							onMouseLeave={() => setHoveredItem(null)}
-						>
-							<div className="relative w-10 h-10">
-								{/* Solid icon */}
-								<item.solidIcon
-									className={`absolute inset-0 transition-opacity duration-200 ${
-										hoveredItem === item.name
-											? "opacity-0"
-											: "opacity-100"
-									}`}
-								/>
-								{/* Outline icon */}
-								<item.outlineIcon
-									className={`absolute inset-0 transition-opacity duration-200 ${
-										hoveredItem === item.name
-											? "opacity-100"
-											: "opacity-0"
-									}`}
-									title={item.name}
-								/>
-							</div>
-						</Link>
-					))}
-				</div>
-			</div>
-			<div className="h-0 md:h-12 text-silver text-md cursor-default text-center hidden md:block">
-				site by me ~{" "}
-				<a
-					className="text-neonpurple hover:text-indigo-600 cursor-pointer underline"
-					href="https://github.com/Blaithnaid/portfolio-next"
-				>
-					view on github
-				</a>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col justify-between min-w-80 bg-gunmetal md:h-screen">
+      <div className="cursor-default font-jersey p-3 text-4xl text-center">
+        <div className="mb-0.5">
+          <a
+            className="text-coral cursor-pointer"
+            href="/home"
+            title="return to home"
+          >
+            iarla sparrow burke
+          </a>
+        </div>
+        <div className="bg-paynesgray h-0.5 w-full rounded-2xl"></div>
+        <div className="text-silver font-poppins text-sm mt-0.5">
+          software developer ~ sound engineer
+        </div>
+      </div>
+      {/* Page title with animation */}
+      <div className="flex flex-nowrap flex-col shrink items-center overflow-hidden justify-center align-center pb-5">
+        <motion.div
+          className="text-white text-center w-fit text-2xl font-poppins cursor-default underline"
+          key={currentPage}
+          initial={{ translateY: -100 }}
+          animate={{ translateY: 0 }}
+          transition={{ duration: 0.3, type: "spring" }}
+        >
+          <b>{currentPage}</b>
+        </motion.div>
+        <div className="flex flex-row items-center gap-2 mt-5">
+          {navigation.map((item) => (
+            <Link
+              key={item.name}
+              href={item.href}
+              className="font-poppins text-white flex items-center gap-2 hover:underline hover:text-white relative"
+              onMouseEnter={() => setHoveredItem(item.name)}
+              onMouseLeave={() => setHoveredItem(null)}
+            >
+              <div className="relative w-10 h-10">
+                {/* Solid icon */}
+                <item.solidIcon
+                  className={`absolute inset-0 transition-opacity duration-200 ${hoveredItem === item.name
+                      ? "opacity-0"
+                      : "opacity-100"
+                    }`}
+                />
+                {/* Outline icon */}
+                <item.outlineIcon
+                  className={`absolute inset-0 transition-opacity duration-200 ${hoveredItem === item.name
+                      ? "opacity-100"
+                      : "opacity-0"
+                    }`}
+                  title={item.name}
+                />
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="h-0 md:h-12 text-silver text-md cursor-default text-center hidden md:block">
+        site by me ~{" "}
+        <a
+          className="text-neonpurple hover:text-indigo-600 cursor-pointer underline"
+          href="https://github.com/Blaithnaid/portfolio-next"
+        >
+          view on github
+        </a>
+      </div>
+    </div>
+  );
 }
